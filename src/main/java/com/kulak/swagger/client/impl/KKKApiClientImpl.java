9ae68d9dce154722b26844db2722bea04746b910
Kanstantsin Kulak <kkulak24@gmail.com>
2020-01-13 21:25:22 +0300

    add logger

--- src/main/java/com/kulak/swagger/client/impl/KKKApiClientImpl.java
+++ src/main/java/com/kulak/swagger/client/impl/KKKApiClientImpl.java
@@ -1,9 +1,12 @@
 package com.kulak.swagger.client.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kulak.swagger.client.KKKApiClient;
 import com.kulak.swagger.entities.Contract;
 import com.kulak.swagger.entities.ContractData;
 import com.kulak.swagger.entities.ContractStatus;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class KKKApiClientImpl implements KKKApiClient {
     @Value("${kkkApiUrl}")
@@ -21,16 +25,29 @@ public class KKKApiClientImpl implements KKKApiClient {
 
     @Override
     public Call<Contract> getContract(final String unp) {
+        log.debug("Getting contract by unp {}", unp);
         return getBaseClient().getContract(unp);
     }
 
     @Override
     public Call<Contract> createContract(final String unp, final ContractData contractData) {
+        log.debug("Create contract by unp {} with next data:", unp);
+        try {
+            log.debug(new ObjectMapper().writeValueAsString(contractData));
+        } catch (JsonProcessingException e) {
+            log.warn("Error logging contract data.");
+        }
         return getBaseClient().createContract(unp, contractData);
     }
 
     @Override
     public Call<Contract> updateContract(final String unp, final ContractStatus status) {
+        log.debug("Update contract by unp {} with next data:", unp);
+        try {
+            log.debug(new ObjectMapper().writeValueAsString(status));
+        } catch (JsonProcessingException e) {
+            log.warn("Error logging contract status.");
+        }
         return getBaseClient().updateContract(unp, status);
     }
 
